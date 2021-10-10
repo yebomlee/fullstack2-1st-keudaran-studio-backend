9952cc3ee41fb1c18ca7fc631b48485c9e0b6fc9
jisue <tjdwltn121@naver.com>
2021-10-10 23:34:30 +0900

    Add: user Id Check 구현완료, err 미들웨어 구현 완료

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -3,6 +3,8 @@ import { userController } from '../controllers';
 
 const router = express.Router();
 
+router.get('/signup/id', userController.checkRealName);
+router.get('/signup', userController.getAlluser);
 router.get('/', userController.getAlluser);
 
 export default router;
