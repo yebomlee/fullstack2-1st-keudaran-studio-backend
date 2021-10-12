385adcd97f278feebe48988d8ec0c0bbc86fddd8
siwon <ksw3721@gmail.com>
2021-10-12 15:28:55 +0900

    Del: memo파일 삭제

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -4,6 +4,7 @@ import { asyncWrapper, errorGenerator } from '../utils';
 const getReviews = asyncWrapper(async (req, res) => {
   const { id, sort, offset, limit } = req.query;
   if (!id) errorGenerator(400, 'NO_ID_WAS_FOUND');
+
   const reviews = await reviewService.getReviews(id, sort, offset, limit);
   res.status(200).json({ message: 'SUCCESS', result: reviews });
 });
