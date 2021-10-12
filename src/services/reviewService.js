be15a5e5b03e3aa465f77c1afe683dbb75a98411
siwon <ksw3721@gmail.com>
2021-10-12 10:32:50 +0900

    Fix: self-refactoring 진행중

--- src/services/reviewService.js
+++ src/services/reviewService.js
@@ -3,11 +3,11 @@ import { reviewDAO } from '../models';
 const getReviews = async (id, sort, offset, limit) => {
   let reviews = [];
 
-  if (sort === 'rating') {
-    reviews = await reviewDAO.getReviewsByRating(id, offset, limit);
-  } else {
-    reviews = await reviewDAO.getReviews(id, offset, limit);
-  }
+  reviews =
+    sort === 'rating'
+      ? await reviewDAO.getReviewsByRating(id, offset, limit)
+      : await reviewDAO.getReviews(id, offset, limit);
+
   return reviews;
 };
 
