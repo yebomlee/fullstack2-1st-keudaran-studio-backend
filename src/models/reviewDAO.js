97f4d3a9fafebef9a4d18e9551cb9540635f13d1
siwon <ksw3721@gmail.com>
2021-10-12 15:25:24 +0900

    Fix: self-refactoring 완료

--- src/models/reviewDAO.js
+++ src/models/reviewDAO.js
@@ -1,7 +1,7 @@
 import prisma from '../../prisma';
 import { Prisma } from '@prisma/client';
 
-const getReviewsByRating = async (id, offset, limit) => {
+const getReviewsByRating = async (id, offset, limit = 10) => {
   let reviews = [];
 
   return (reviews = await prisma.$queryRaw`
@@ -11,18 +11,27 @@ const getReviewsByRating = async (id, offset, limit) => {
   LEFT JOIN review_images ON reviews.id = review_images.review_id
   LEFT JOIN users ON reviews.user_id = users.id
   LEFT JOIN products ON reviews.product_id = products.id
-  WHERE reviews.product_id=3
-  ORDER BY rating DESC;
-    ${offset ? Prisma.sql`LIMIT ${offset}, ${limit}` : Prisma.empty};`);
+  WHERE reviews.product_id=${id}
+  ORDER BY rating DESC
+  LIMIT ${limit}
+  ${offset ? Prisma.sql`OFFSET ${offset}` : Prisma.empty};
+  `);
 };
 
-const getReviews = async (id, offset, limit) => {
+const getReviews = async (id, offset, limit = 10) => {
   let reviews = [];
   return (reviews = await prisma.$queryRaw`
-    SELECT * 
-    FROM reviews WHERE product_id=${id} 
-    ORDER BY created_at DESC 
-    ${offset ? Prisma.sql`Limit ${offset}, ${limit}` : Prisma.empty};`);
+  SELECT reviews.id, users.username, reviews.product_id, products.id, products.name,
+  reviews.rating, reviews.content, reviews.created_at, reviews.updated_at, review_images.image_url
+  FROM reviews
+  LEFT JOIN review_images ON reviews.id = review_images.review_id
+  LEFT JOIN users ON reviews.user_id = users.id
+  LEFT JOIN products ON reviews.product_id = products.id
+  WHERE reviews.product_id=${id}
+  ORDER BY created_at DESC
+  LIMIT ${limit}
+  ${offset ? Prisma.sql`OFFSET ${offset}` : Prisma.empty};
+  `);
 };
 
 export default { getReviews, getReviewsByRating };
--- src/utils/index 2.js
+++
@@ -1,6 +0,0 @@
-import errorGenerator from './errorGenerator';
-import asyncWrapper from './asyncWrapper';
-import jwt from './jwt';
-import bcrypt from './bcrypt';
-
-export { errorGenerator, asyncWrapper, jwt, bcrypt };
