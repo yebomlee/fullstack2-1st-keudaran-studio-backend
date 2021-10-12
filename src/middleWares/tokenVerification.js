977c5e711cc7dc6f4b6374adc1dbbfdd55c655f6
ebong <eebzaaa@gmail.com>
2021-10-12 13:22:17 +0900

    Add: sign in API 구현 완료

--- src/middleWares/tokenVerification.js
+++ src/middleWares/tokenVerification.js
@@ -1,16 +1,15 @@
+import { errorGenerator, asyncWrapper } from '../utils';
 import jwt from '../utils/jwt';
 
-const tokenVerifiation = async (req, res, next) => {
-  try {
-    const token = req.cookies.user;
-    if (token) {
-      const payload = await jwt.verifyToken(token);
-      req.body.userId = payload.id;
-    }
-    next();
-  } catch (err) {
-    res.status(401).json({ message: 'INVALID_TOKEN' });
+const tokenVerification = asyncWrapper(async (req, res, next) => {
+  const token = req.cookies.user;
+  if (token) {
+    const payload = await jwt.verifyToken(token);
+    req.body.userId = payload.id;
+  } else {
+    errorGenerator(401, 'INVALID_TOKEN');
   }
-};
+  next();
+});
 
-export default tokenVerifiation;
+export default tokenVerification;
